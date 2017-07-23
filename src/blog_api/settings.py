7165af7133eb171e514ba2a3190414984a00c0b1
younes-bh <benhoumich.younes@gmail.com>
2017-07-23 18:53:34 +0200

    add ListAPIVIew and a modelSerializer

--- src/blog_api/settings.py
+++ src/blog_api/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Django rest framework
+    'rest_framework',
+
     # third party
     'crispy_forms',
     'markdown_deux',
