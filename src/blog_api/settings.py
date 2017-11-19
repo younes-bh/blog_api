a6d848518ff5b366a5b0b71efd58b8fd45358668
younux <benhoumich.younes@gmail.com>
2017-11-19 19:41:27 +0100

    Revert "add SessionAuthentication"
    This reverts commit ec9e16021bb239991f7cc77eb656c3154eca4f94.

--- src/blog_api/settings.py
+++ src/blog_api/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
     # ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         # see http://www.django-rest-framework.org/api-guide/authentication/
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         # 'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.TokenAuthentication',
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
